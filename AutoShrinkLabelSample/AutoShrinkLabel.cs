3e01234984d2887084279f08ce5e257f25bdd69e
aquarla <aquarla@aquarla.net>
2012-02-28 13:24:44 +0900

    initial import

---
+++ AutoShrinkLabelSample/AutoShrinkLabel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AutoShrinkLabelSample
+{
+    public partial class AutoShrinkLabel : Label
+    {
+        public AutoShrinkLabel()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            Graphics g = pe.Graphics;
+            Font realFont = this.Font;
+
+            SizeF size = g.MeasureString(this.Text, realFont);
+            while (size.Width >= this.Width || size.Height >= this.Height)
+            {
+                if (realFont.Size <= 1.0f)
+                    break;
+
+                realFont = new Font(realFont.FontFamily, realFont.Size - 1.0f);
+                size = g.MeasureString(this.Text, realFont);
+            }
+
+            TextFormatFlags flags = TextFormatFlags.VerticalCenter | TextFormatFlags.HorizontalCenter;
+            TextRenderer.DrawText(g, this.Text, realFont, new Rectangle(0, 0, (int)size.Width, (int)size.Height), this.ForeColor, flags);
+        }
+    }
+}
---
+++ AutoShrinkLabelSample/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace AutoShrinkLabelSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナ変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナで生成されたコード
+
+        /// <summary>
+        /// デザイナ サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディタで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.autoShrinkLabel1 = new AutoShrinkLabelSample.AutoShrinkLabel();
+            this.SuspendLayout();
+            // 
+            // autoShrinkLabel1
+            // 
+            this.autoShrinkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.autoShrinkLabel1.Font = new System.Drawing.Font("MS UI Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.autoShrinkLabel1.Location = new System.Drawing.Point(12, 9);
+            this.autoShrinkLabel1.Name = "autoShrinkLabel1";
+            this.autoShrinkLabel1.Size = new System.Drawing.Size(325, 86);
+            this.autoShrinkLabel1.TabIndex = 0;
+            this.autoShrinkLabel1.Text = "ラベルラベル";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 104);
+            this.Controls.Add(this.autoShrinkLabel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AutoShrinkLabel autoShrinkLabel1;
+    }
+}
+
